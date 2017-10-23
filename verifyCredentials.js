bd2be83d225efc75a01113a05156799f1a466227
Alp Bilgin <alp_bilgin@hotmail.com>
2017-10-23 11:48:47 +0200

    adding trace debug output to verifyCredentials

--- verifyCredentials.js
+++ verifyCredentials.js
@@ -5,27 +5,31 @@ module.exports = function verifyCredentials(credentials, cb) {
     console.log('Credentials passed for verification %j', credentials);
 
     //
-    this.http = Axios.create({
+    const http = Axios.create({
         baseURL: 'https://pst.m2mgo.com/api',
     });
+
     const payload = {
         "Email": credentials.email,
         "Password": credentials.password
     }
 
+    console.log('POST req payload %j', payload);
 
-    this.http.post('/cms/membership-user/token', payload, {
-            responseType: 'json'
-        }).then(function (response) {
-            console.log(response);
-            cb(null, {
-                verified: true
-            });
-        })
-        .catch(function (error) {
-            console.log(error);
-            cb(null, {
-                verified: false
-            });
+
+    http.post('/cms/membership-user/token', payload, {
+        responseType: 'json'
+    }).then(function (response) {
+        console.log('a');
+        console.log(response);
+        cb(null, {
+            verified: true
         });
+    }).catch(function (error) {
+        console.log('b');
+        console.log(error);
+        cb(null, {
+            verified: false
+        });
+    });
 };
